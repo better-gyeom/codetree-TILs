e12881abd7a4f455b7dfe42bedc817b4f1d3c50e
Gyeom <125348113+better-gyeom@users.noreply.github.com>
2024-03-05 21:59:54 +0900

    Title: 방향에 맞춰 이동, Time: 166ms, Memory: 10MB, Status: Passed - CodeTree

--- 240305/방향에 맞춰 이동/move-in-direction.java
+++ 240305/방향에 맞춰 이동/move-in-direction.java
@@ -1,5 +1,37 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
+        Scanner input = new Scanner(System.in);
+
+        int N = input.nextInt();
+        int x = 0; 
+        int y = 0;
+
+        int[] dx = new int[]{0, -1, 0, 1};
+        int[] dy = {1, 0, -1, 0};
+
+        for (int i = 0; i < N; i++) {
+            String location = input.next();
+            int distance = input.nextInt();
+
+            if (location.equals("N")) {
+                x = x + (dx[0] * distance);
+                y = y + (dy[0] * distance);
+            } else if (location.equals("E")) {
+                x = x + (dx[3] * distance);
+                y = y + (dy[3] * distance);
+            } else if (location.equals("S")) {
+                x = x + (dx[2] * distance);
+                y = y + (dy[2] * distance);
+            } else if (location.equals("W")) {
+                x = x + (dx[1] * distance);
+                y = y + (dy[1] * distance);
+            }
+            
+        }
+        System.out.print(x + " ");
+        System.out.println(y);
     }
 }
