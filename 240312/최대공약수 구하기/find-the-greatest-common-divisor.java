6141b239c2344da06f22dbf3b997f369d151a225
Gyeom <125348113+better-gyeom@users.noreply.github.com>
2024-03-12 12:32:10 +0900

    Title: 최대공약수 구하기, Time: 123ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240312/최대공약수 구하기/find-the-greatest-common-divisor.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+        findGCD(n, m);
+    }
+
+    public static void findGCD(int n, int m) {
+        int common = 0;
+        
+        for (int i = 2; i <= Math.min(n, m); i++) {
+                if (n % i == 0 && m % i == 0) {
+                    common = i;
+                } //손으로 풀땐 약수들을 곱해가지만
+                //코드로 풀땐 계속 증가시키면서 나누어지는 가장 큰수를 찾으면 된다..!
+                
+            }
+        
+        System.out.println(common);
+    }
+}
