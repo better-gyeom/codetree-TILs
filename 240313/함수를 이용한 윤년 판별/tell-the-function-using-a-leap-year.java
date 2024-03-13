d544a23871661ee84f7c3104e4d2a8640c414255
Gyeom <125348113+better-gyeom@users.noreply.github.com>
2024-03-13 12:22:39 +0900

    Title: 함수를 이용한 윤년 판별, Time: 143ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240313/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner input = new Scanner(System.in);
+
+        int year = input.nextInt();
+
+        if (isLeafYear(year)) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
+    }
+
+    public static boolean isLeafYear(int year) {
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0) {
+                    return true;
+                } else {
+                    return false;
+                }
+            } else {
+                return true;
+            }
+        } else {
+            return false;
+        }
+    }
+}
