50b6bd927c506b8765c8737b03891cd2a41c1fa4
Gyeom <125348113+better-gyeom@users.noreply.github.com>
2024-03-13 12:14:00 +0900

    Title: 함수를 이용한 소수 판별, Time: 148ms, Memory: 10MB, Status: Passed - CodeTree

--- 240313/함수를 이용한 소수 판별/decimal-decisions-using-functions.java
+++ 240313/함수를 이용한 소수 판별/decimal-decisions-using-functions.java
@@ -9,7 +9,7 @@ public class Main {
 
         int sumV = 0;
         for (int i = a; i <= b; i++) {
-            if (isPrime(i)) {
+            if (i != 1 && isPrime(i)) {
                 sumV += i;
             }
         }
