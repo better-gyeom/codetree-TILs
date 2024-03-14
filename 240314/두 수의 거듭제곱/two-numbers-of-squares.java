a9e3a9916650fe9a9b11b544ab040d2ae99c5343
Gyeom <125348113+better-gyeom@users.noreply.github.com>
2024-03-14 13:18:23 +0900

    Title: 두 수의 거듭제곱, Time: 119ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240314/두 수의 거듭제곱/two-numbers-of-squares.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+        int b = input.nextInt();
+
+        System.out.println(pow(a, b));
+    }
+
+    public static int pow(int a, int b) {
+        return (int) Math.pow(a, b);
+    }
+}
