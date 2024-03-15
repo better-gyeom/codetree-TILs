9c628d9b9957fb79e404fd6f01f67dce7a7efa19
Gyeom <125348113+better-gyeom@users.noreply.github.com>
2024-03-15 19:25:39 +0900

    Title: 2021년 날짜의 유무, Time: 141ms, Memory: 10MB, Status: Passed - CodeTree

--- 240315/2021년 날짜의 유무/with-or-without-2021.java
+++ 240315/2021년 날짜의 유무/with-or-without-2021.java
@@ -16,8 +16,52 @@ public class Main {
     }
 
     public static boolean isExist(int m, int d) {
-        if (1 <= m && m <= 12) {
-            if ((1 <= d && d <= 28) || d == 30 || d == 31) {
+        if (m == 1) {
+            if (1 <= d && d <= 31) {
+                return true;
+            }
+        } else if (m == 2) {
+            if (1 <= d && d <= 28) {
+                return true;
+            }
+        } else if (m == 3) {
+            if (1 <= d && d <= 31) {
+                return true;
+            }
+        } else if (m == 4) {
+            if (1 <= d && d <= 30) {
+                return true;
+            }
+        } else if (m == 5) {
+            if (1 <= d && d <= 31) {
+                return true;
+            }
+        } else if (m == 6) {
+            if (1 <= d && d <= 30) {
+                return true;
+            }
+        } else if (m == 7) {
+            if (1 <= d && d <= 31) {
+                return true;
+            }
+        } else if (m == 8) {
+            if (1 <= d && d <= 31) {
+                return true;
+            }
+        } else if (m == 9) {
+            if (1 <= d && d <= 30) {
+                return true;
+            }
+        } else if (m == 10) {
+            if (1 <= d && d <= 31) {
+                return true;
+            }
+        } else if (m == 11) {
+            if (1 <= d && d <= 30) {
+                return true;
+            }
+        } else if (m == 12) {
+            if (1 <= d && d <= 31) {
                 return true;
             }
         }
