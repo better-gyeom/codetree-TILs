a21ca74d2ef1c4172fb3d284b532128c163af2d9
Gyeom <125348113+better-gyeom@users.noreply.github.com>
2024-03-25 16:03:08 +0900

    Title: 중앙값 계산 2, Time: 146ms, Memory: 10MB, Status: Passed - CodeTree

--- 240325/중앙값 계산 2/get-median-2.java
+++ 240325/중앙값 계산 2/get-median-2.java
@@ -16,9 +16,10 @@ public class Main {
 
     public static void mid(int n, int[] arr) {
         for (int i = 0; i < n; i++) {
+            
             if (i % 2 == 0) { //홀수번째 수는 실제로 짝수 인덱스이기 때문에
-                Arrays.sort(arr, 0, i); //
-                System.out.println(Arrays.toString(arr));
+                Arrays.sort(arr, 0, i + 1); //정렬도 i + 1까지 해야함
+
                 System.out.print(arr[i / 2] + " ");
             }
         }
