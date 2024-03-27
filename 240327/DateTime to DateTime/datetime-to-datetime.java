7d14d6910217ef1505fbdfedb09bd3fee91316dd
Gyeom <125348113+better-gyeom@users.noreply.github.com>
2024-03-27 12:49:39 +0900

    Title: DateTime to DateTime, Time: 144ms, Memory: 10MB, Status: Passed - CodeTree

--- 240327/DateTime to DateTime/datetime-to-datetime.java
+++ 240327/DateTime to DateTime/datetime-to-datetime.java
@@ -13,19 +13,20 @@ public class Main {
     }
 
     public static int how(int a, int b, int c) {
+        if (a == 11) {
+            if (b < 11) {
+                return -1;
+            }
 
-        if (b < 11) {
-            return -1;
+            if (c < 11) {
+                return -1;
+            }
         }
-
-        if (c < 11) {
-            return -1;
-        }
-
+        
         int days = a - 11;
         int hours = b - 11;
         int minutes = c - 11;
-        
+
         return (days * 24 * 60) + (hours * 60) + minutes;
     }
 }
