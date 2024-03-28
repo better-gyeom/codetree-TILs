53ac888cad198734cb5a140b8fdc1bf6d321f3d0
Gyeom <125348113+better-gyeom@users.noreply.github.com>
2024-03-28 15:26:25 +0900

    Title: 최대로 겹치는 구간, Time: 155ms, Memory: 10MB, Status: Passed - CodeTree

--- 240328/최대로 겹치는 구간/maximum-overlapped-segments.java
+++ 240328/최대로 겹치는 구간/maximum-overlapped-segments.java
@@ -6,12 +6,25 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        int[] arr = new int[101];
+        int[][] xy = new int[n][2];
         for (int i = 0; i < n; i++) {
-            int x1 = sc.nextInt();
-            int x2 = sc.nextInt();
+            xy[i][0] = sc.nextInt();
+            xy[i][1] = sc.nextInt();
+        }
 
-            for (int j = x1; j < x2; j++) {
+        int minV = 101;
+        for (int i = 0; i < xy.length; i++) {
+            minV = Math.min(Math.min(xy[i][0], xy[i][1]), minV);
+        }
+
+        for (int i = 0; i < xy.length; i++) {
+            xy[i][0] += Math.abs(minV);
+            xy[i][1] += Math.abs(minV);
+        }
+
+        int[] arr = new int[202];
+        for (int i = 0; i < xy.length; i++) {
+            for (int j = xy[i][0]; j < xy[i][1]; j++) {
                 arr[j] += 1;
             }
         }
