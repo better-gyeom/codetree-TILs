5a08a9188220e96db297a47ba4c6b0d6a04c880c
Gyeom <125348113+better-gyeom@users.noreply.github.com>
2024-04-01 21:06:46 +0900

    Title: 신기한 타일 뒤집기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240401/신기한 타일 뒤집기/strange-flipping-tiles.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[] colored = new int[200001];
+
+        int now = 1000;
+        for (int i = 0; i < n; i++) {
+            int dis = sc.nextInt();
+            String dir = sc.next();
+
+            if (dir.charAt(0) == 'R') {
+                for (int j = now; j < now + dis; j++) {
+                    colored[j] = 1;
+                }
+                now = now + dis - 1;
+            } else {
+                for (int j = now - dis + 1; j < now + 1; j++) {
+                    
+                    colored[j] = 2;
+                }
+                now = now - dis + 1;
+            }
+        }
+
+        int b = 0;
+        int w = 0;
+        for (int i = 0; i < colored.length; i++) {
+            if (colored[i] == 1) {
+                b++;
+            } else if (colored[i] == 2) {
+                w++;
+            } else {
+                continue;
+            }
+        }
+
+        System.out.println(w + " " + b);
+    }
+}
