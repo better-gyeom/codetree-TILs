424192123fb0aba66892ef6ad01e2c0d6253a11a
Gyeom <125348113+better-gyeom@users.noreply.github.com>
2024-04-02 14:17:12 +0900

    Title: 연속되는 수 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240402/연속되는 수 2/continuous-number2.java
+++ 240402/연속되는 수 2/continuous-number2.java
@@ -13,7 +13,7 @@ public class Main {
 
         int maxV = 0;
         int cnt = 1;
-        for (int i = 1; i < arr.length; i++) {
+        for (int i = 0; i < arr.length; i++) {
             if (arr[i] == arr[i - 1]) {
                 cnt++;
             }
