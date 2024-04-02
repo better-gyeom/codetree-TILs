c1565b42b56ffc03ca7574ce249254bc7d1f1d91
Gyeom <125348113+better-gyeom@users.noreply.github.com>
2024-04-02 13:46:30 +0900

    Title: 계속 중첩되는 사각형, Time: 135ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240402/계속 중첩되는 사각형/continuously-overlapping-squares.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[][] arr = new int[201][201];
+        for (int i = 0; i < n; i++) {
+            int x1 = sc.nextInt() + 100;
+            int y1 = sc.nextInt() + 100;
+            int x2 = sc.nextInt() + 100;
+            int y2 = sc.nextInt() + 100;
+
+            if (i % 2 == 0) {
+                //red
+                for (int x = x1; x < x2; x++) {
+                    for (int y = y1; y < y2; y++) {
+                        arr[x][y] = 1;
+                    }
+                }
+            } else {
+                //blue
+                for (int x = x1; x < x2; x++) {
+                    for (int y = y1; y < y2; y++) {
+                        arr[x][y] = 2;
+                    }
+                }
+            }
+        }
+
+        int cnt = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                if (arr[i][j] == 2) {
+                    cnt++;
+                }
+            }
+        }
+
+        System.out.println(cnt);
+    }
+}
